1ae6f616b3ded4962901ce99efcb6c4be947a1e5
pobu168 <244202969@qq.com>
2021-01-26 16:31:38 +0800

    update router config

--- ui/src/router-plugin.js
+++ ui/src/router-plugin.js
@@ -1,5 +1,3 @@
-import Vue from 'vue'
-import Router from 'vue-router'
 import Index from '@/pages/index'
 import terminalOperation from '@/pages/terminal-operation'
 import terminalManagement from '@/pages/terminal-management'
@@ -7,52 +5,50 @@ import permissions from '@/pages/management/permissions'
 import sessionRecords from '@/pages/management/session-records'
 import transferRecords from '@/pages/management/transfer-records'
 
-Vue.use(Router)
-export default new Router({
-  routes: [
-    {
-      path: '/terminalOperation',
-      name: 'terminalOperation',
-      redirect: 'terminalOperation',
-      component: Index,
-      children: [
-        {
-          path: '/terminalOperation',
-          name: 'terminalOperation',
-          component: terminalOperation,
-          params: {},
-          props: true
-        }
-      ]
-    },
-    {
-      path: '/terminalManagement',
-      name: 'terminalManagement',
-      redirect: '/terminalManagement/sessionRecords',
-      component: terminalManagement,
-      children: [
-        {
-          path: 'permissions',
-          name: 'permissions',
-          title: '文件传输权限',
-          meta: {},
-          component: permissions
-        },
-        {
-          path: 'sessionRecords',
-          name: 'sessionRecords',
-          title: '访问记录',
-          meta: {},
-          component: sessionRecords
-        },
-        {
-          path: 'transferRecords',
-          name: 'transferRecords',
-          title: '文件传输列表',
-          meta: {},
-          component: transferRecords
-        }
-      ]
-    }
-  ]
-})
+const router = [
+  {
+    path: '/terminalOperation',
+    name: 'terminalOperation',
+    redirect: 'terminalOperation',
+    component: Index,
+    children: [
+      {
+        path: '/terminalOperation',
+        name: 'terminalOperation',
+        component: terminalOperation,
+        params: {},
+        props: true
+      }
+    ]
+  },
+  {
+    path: '/terminalManagement',
+    name: 'terminalManagement',
+    redirect: '/terminalManagement/sessionRecords',
+    component: terminalManagement,
+    children: [
+      {
+        path: 'permissions',
+        name: 'permissions',
+        title: '文件传输权限',
+        meta: {},
+        component: permissions
+      },
+      {
+        path: 'sessionRecords',
+        name: 'sessionRecords',
+        title: '访问记录',
+        meta: {},
+        component: sessionRecords
+      },
+      {
+        path: 'transferRecords',
+        name: 'transferRecords',
+        title: '文件传输列表',
+        meta: {},
+        component: transferRecords
+      }
+    ]
+  }
+]
+export default router
