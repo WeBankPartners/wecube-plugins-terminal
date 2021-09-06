098f3bcf98e7a29ee989bc9ebacce004e90add92
royl88 <roywu@webank.com>
2021-09-06 12:15:11 +0800

    #26 update static resource route

--- api/terminal/terminal/apps/assets/route.py
+++ api/terminal/terminal/apps/assets/route.py
@@ -8,6 +8,12 @@ from terminal.apps.assets import controller
 
 def add_routes(api):
     api.add_static_route('/', CONF.ui, fallback_filename='index.html')
+    api.add_static_route('/terminal', CONF.ui)
+    api.add_static_route('/terminal/css', CONF.ui + '/css')
+    api.add_static_route('/terminal/fonts', CONF.ui + '/fonts')
+    api.add_static_route('/terminal/img', CONF.ui + '/img')
+    api.add_static_route('/terminal/js', CONF.ui + '/js')
+    api.add_static_route('/terminal/xtem-player', CONF.ui + '/xtem-player')
     api.add_route('/terminal/v1/assets', controller.CollectionAssets())
     api.add_route('/terminal/v1/view-assets', controller.CollectionViewAssets())
     api.add_route('/terminal/v1/assets/{rid}/file', controller.ItemAssetFile())
