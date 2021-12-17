5c3deb876de88e6f2390ffcbec42d3d06d52af00
royl88 <576477085@qq.com>
2021-12-17 15:12:44 +0800

    outsource log level

--- api/terminal/terminal/server/scheduler.py
+++ api/terminal/terminal/server/scheduler.py
@@ -21,6 +21,7 @@ from talos.core import logging as mylogger
 from apscheduler.schedulers.blocking import BlockingScheduler
 from apscheduler.jobstores.memory import MemoryJobStore
 from apscheduler.executors.pool import ThreadPoolExecutor
+from terminal.server.wsgi_server import application
 
 CONF = config.CONF
 LOG = logging.getLogger(__name__)
@@ -92,18 +93,10 @@ def rotate_log():
 
 
 def main():
-    config.setup(os.environ.get('TERMINAL_CONF', '/etc/terminal/terminal.conf'),
-                 dir_path=os.environ.get('TERMINAL_CONF_DIR', '/etc/terminal/terminal.conf.d'))
-    mylogger.setup()
     tz_info = timezone(CONF.timezone)
     try:
         if CONF.platform_timezone:
-            prefix = 'ENV@'
-            value = CONF.platform_timezone
-            if value.startswith(prefix):
-                env_name = value[len(prefix):]
-                value = os.getenv(env_name, default='')
-            tz_info = timezone(value)
+            tz_info = timezone(CONF.platform_timezone)
     except Exception as e:
         LOG.exception(e)
     scheduler = BlockingScheduler(jobstores=jobstores,
